ea5b0921fe5d62640f0bdbd15e8d89f730a55cf4
Julian Sotelo <juliansotelo@mail.fresnostate.edu>
2023-10-09 13:00:53 -0700

    added data, paths, and more functions for Santa Ana to parse accepted projects in the last 6 months

---
+++ src/const.py
@@ -0,0 +1,6 @@
+# This portion of code contains the constants used in the project to fill null values 
+# Below are listed city emails and phone numbers
+
+SANTA_ANA_PLANNING_OFFICE_PHONE = '714-667-2700'
+SANTA_ANA_PLANNING_OFFICE_EMAIL = 'planning@santa-ana.org'
+SANTA_ANA_PLANNING_URL = 'https://www.santa-ana.org/major-planning-projects-and-monthly-development-project-reports/'
