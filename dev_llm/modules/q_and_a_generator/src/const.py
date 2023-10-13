8d111ee684bca9e8844fd17e2996193efff1cf29
Julian Sotelo <juliansotelo@mail.fresnostate.edu>
2023-10-12 20:43:57 -0700

    modified constants and added scripts

--- dev_llm/modules/q_and_a_generator/src/const.py
+++ dev_llm/modules/q_and_a_generator/src/const.py
@@ -1,1 +1,7 @@
-UPCODES_URL = "https://up.codes/codes/california"
+
+
+CALIFORNIA_UPCODES_URL = "https://up.codes/codes/california"
+LOS_ANGELES_UPCODES_URL = "https://up.codes/codes/los_angeles"
+LOS_ANGELES_COUNTY_UPCODES_URL = "https://up.codes/codes/los-angeles-county"
+SAN_FRANCISCO_UPCODES_URL = "https://up.codes/codes/san_francisco"
+SAN_JOSE_UPCODES_URL = "https://up.codes/codes/san-jose"
--- dev_llm/modules/q_and_a_generator/src/paths.py
+++ dev_llm/modules/q_and_a_generator/src/paths.py
@@ -5,5 +5,20 @@ PARENT_DIR = Path(__file__).parent.resolve().parent
 
 DATA_DIR = PARENT_DIR / "data"
 
-if not DATA_DIR.exists():
-    os.makedir(DATA_DIR)
+RAW_DATA_DIR = DATA_DIR / "raw"
+
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+# Ensure directories exist, and if not, create them
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+if not Path.exists(DATA_DIR):
+    os.mkdir(DATA_DIR)
+
+if not Path.exists(RAW_DATA_DIR):
+    os.mkdir(RAW_DATA_DIR)
+
+if not Path.exists(PROCESSED_DATA_DIR):
+    os.mkdir(PROCESSED_DATA_DIR)
